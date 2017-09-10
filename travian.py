7b6ab1b5f9340f47e59c45ba9827cb5dea2ceea6
HankChow <280630620@qq.com>
2017-09-10 17:35:25 +0800

    资源和资源田获取模块完成

--- travian.py
+++ travian.py
@@ -39,6 +39,20 @@ class Travian(object):
         self.driver.implicitly_wait(10)
         print('Successfully logged in.')
 
+    # 需要设为装饰器，未测试
+    def switch_to_dorf1(self):
+        if 'dorf1.php' not in self.driver.current_url:
+            ul_navigation = self.driver.find_element_by_css_selector('ul#navigation')
+            ul_navigation.find_element_by_css_selector('li#n1').find_element_by_tag_name('a').click()
+            self.driver.implicitly_wait(5)
+
+    # 需要设为装饰器，未测试
+    def switch_to_dorf2(self):
+        if 'dorf2.php' not in self.driver.current_url:
+            ul_navigation = self.driver.find_element_by_css_selector('ul#navigation')
+            ul_navigation.find_element_by_css_selector('li#n2').find_element_by_tag_name('a').click()
+            self.driver.implicitly_wait(5)
+
     def get_current_village_production(self):
         tbl_production = self.driver.find_element_by_css_selector('table#production')
         production_list = tbl_production.find_element_by_xpath('tbody').find_elements_by_xpath('tr')
@@ -47,6 +61,45 @@ class Travian(object):
             cvp[item.find_element_by_class_name('res').text.strip(':')] = \
                 int(item.find_element_by_class_name('num').text.strip('\u202d\u202c'))
         return cvp
+
+    def get_current_village_troops(self):
+        tbl_troops = self.driver.find_element_by_css_selector('table#troops')
+        troops_list = tbl_troops.find_element_by_xpath('tbody').find_elements_by_xpath('tr')
+        cvt = {}
+        for item in troops_list:
+            cvt[item.find_element_by_class_name('un').text.strip()] = \
+                int(item.find_element_by_class_name('num').text.strip('\u202d\u202c'))
+        return cvt
+
+    def get_current_village_resources(self):
+        ul_stockbar = self.driver.find_element_by_css_selector('ul#stockBar')
+        warehouse_limit = ul_stockbar.find_element_by_css_selector('span#stockBarWarehouse').text.strip('\u202d\u202c')
+        granary_limit = ul_stockbar.find_element_by_css_selector('span#stockBarGranary').text.strip('\u202d\u202c')
+        lumber = ul_stockbar.find_element_by_css_selector('span#l1').text.strip('\u202d\u202c')
+        clay = ul_stockbar.find_element_by_css_selector('span#l2').text.strip('\u202d\u202c')
+        iron = ul_stockbar.find_element_by_css_selector('span#l3').text.strip('\u202d\u202c')
+        crop = ul_stockbar.find_element_by_css_selector('span#l4').text.strip('\u202d\u202c')
+        free = ul_stockbar.find_element_by_css_selector('span#stockBarFreeCrop').text.strip('\u202d\u202c')
+        cvr = {
+            'warehouse': warehouse_limit,
+            'granary': granary_limit,
+            'lumber': lumber,
+            'clay': clay,
+            'iron': iron,
+            'crop': crop,
+            'free': free
+        }
+        return cvr
+
+    def get_current_village_fields(self):
+        field_map = self.driver.find_element_by_css_selector('map#rx')
+        fields = field_map.find_elements_by_css_selector('area')
+        cvf = list(map(lambda x: {
+            'id': x.get_attribute('href').split('=')[1],
+            'type': x.get_attribute('alt').split(' Level ')[0],
+            'level': x.get_attribute('alt').split(' Level ')[1]
+        }, list(filter(lambda x: 'id' in x.get_attribute('href'), fields))))
+        return cvf
 
     def logout(self):
         btn_logout = self.driver.find_element_by_xpath('//a[@href="logout.php"]')
@@ -61,5 +114,8 @@ if __name__ == '__main__':
     t.init_webdriver()
     t.login()
     print(t.get_current_village_production())
+    print(t.get_current_village_troops())
+    print(t.get_current_village_resources())
+    print(t.get_current_village_fields())
     t.logout()
     t.close_browser()
