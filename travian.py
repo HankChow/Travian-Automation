62f7a5a28e5386e722b0c93044553713979ca47f
HankChow <280630620@qq.com>
2017-09-10 16:02:44 +0800

    修复在phantomjs下元素不可见的问题

--- travian.py
+++ travian.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import time
-from bs4 import BeautifulSoup as bs
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class Travian(object):
 
@@ -13,13 +12,15 @@ class Travian(object):
         self.server = server
 
     def init_webdriver(self):
-        self.driver = webdriver.Firefox()
+        #self.driver = webdriver.Firefox()
+        self.driver = webdriver.PhantomJS()
 
     def login(self):
         self.driver.get('https://www.travian.com/international')
         self.driver.implicitly_wait(20)
-        btn_login = self.driver.find_element_by_xpath('//a[@href="#login"]')
-        btn_login.click()
+        # 首页的 LOGIN 按钮在 phantomjs 引擎下会谜之不可见
+        # 因此无法用常规的 click() 来模拟点击，只能用原生 js 定位这个不可见元素后点击
+        self.driver.execute_script('document.evaluate(\'//a[@href="#login"]\', document, null, XPathResult.ANY_TYPE, null).iterateNext().click();')
         # 服务器列表
         world_list = self.driver.find_element_by_class_name('worldGroup').find_elements_by_xpath('div')
         for item in world_list:
@@ -35,16 +36,30 @@ class Travian(object):
         input_password.send_keys(self.password)
         btn_submit_login = self.driver.find_element_by_id('id')
         btn_submit_login.click()
+        self.driver.implicitly_wait(10)
+        print('Successfully logged in.')
+
+    def get_current_village_production(self):
+        tbl_production = self.driver.find_element_by_css_selector('table#production')
+        production_list = tbl_production.find_element_by_xpath('tbody').find_elements_by_xpath('tr')
+        cvp = {}
+        for item in production_list:
+            cvp[item.find_element_by_class_name('res').text.strip(':')] = \
+                int(item.find_element_by_class_name('num').text.strip('\u202d\u202c'))
+        return cvp
 
     def logout(self):
-        
+        btn_logout = self.driver.find_element_by_xpath('//a[@href="logout.php"]')
+        btn_logout.click()
+        print('Successfully logged out.')
 
     def close_browser(self):
         self.driver.quit()
 
 if __name__ == '__main__':
-    t = Travian('', '', '')
+    t = Travian('hank47', 'zc_7r4v14n', 'com 7')
     t.init_webdriver()
     t.login()
-    time.sleep(30)
+    print(t.get_current_village_production())
+    t.logout()
     t.close_browser()
