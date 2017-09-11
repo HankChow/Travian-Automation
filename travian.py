08103144c5c21b860ab881b76f346fa4fa9c6676
HankChow <280630620@qq.com>
2017-09-11 21:54:31 +0800

    内部建筑模块、升级队列模块完成

--- travian.py
+++ travian.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+from bs4 import BeautifulSoup
+from pprint import pprint
 from selenium import webdriver
 
 
@@ -38,19 +40,37 @@ class Travian(object):
         self.driver.implicitly_wait(10)
         print('Successfully logged in.')
 
-    # 需要设为装饰器，未测试
-    def switch_to_dorf1(self):
-        if 'dorf1.php' not in self.driver.current_url:
-            ul_navigation = self.driver.find_element_by_css_selector('ul#navigation')
-            ul_navigation.find_element_by_css_selector('li#n1').find_element_by_tag_name('a').click()
-            self.driver.implicitly_wait(5)
+    # 装饰器
+    def switch_to_dorf1(self, func):
+        def wrapper(*args, **kwargs):
+            if 'dorf1.php' not in self.driver.current_url:
+                ul_navigation = self.driver.find_element_by_css_selector('ul#navigation')
+                ul_navigation.find_element_by_css_selector('li#n1').find_element_by_tag_name('a').click()
+                self.driver.implicitly_wait(5)
+            return func(*args, **kwargs)
+        return wrapper()
 
-    # 需要设为装饰器，未测试
-    def switch_to_dorf2(self):
-        if 'dorf2.php' not in self.driver.current_url:
-            ul_navigation = self.driver.find_element_by_css_selector('ul#navigation')
-            ul_navigation.find_element_by_css_selector('li#n2').find_element_by_tag_name('a').click()
-            self.driver.implicitly_wait(5)
+    # 装饰器
+    def switch_to_dorf2(self, func):
+        def wrapper(*args, **kwargs):
+            if 'dorf2.php' not in self.driver.current_url:
+                ul_navigation = self.driver.find_element_by_css_selector('ul#navigation')
+                ul_navigation.find_element_by_css_selector('li#n2').find_element_by_tag_name('a').click()
+                self.driver.implicitly_wait(5)
+            return func(*args, **kwargs)
+        return wrapper()
+
+    def get_villages(self):
+        village_box = self.driver.find_element_by_css_selector('div#sidebarBoxVillagelist')
+        box_content = village_box.find_element_by_css_selector('div.content')
+        villages = box_content.find_elements_by_css_selector('li')
+        vlg = list(map(lambda x: {
+            'current': True if x.get_attribute('class').strip() == 'active' else False,
+            'name': x.find_element_by_css_selector('div.name').text.strip(),
+            'coordsX': int(x.find_element_by_css_selector('span.coordinateX').text[1:].strip('\u202d\u202c')),
+            'coordsY': int(x.find_element_by_css_selector('span.coordinateY').text[:-1].strip('\u202d\u202c'))
+        }, villages))
+        return vlg
 
     def get_current_village_production(self):
         tbl_production = self.driver.find_element_by_css_selector('table#production')
@@ -94,11 +114,43 @@ class Travian(object):
         field_map = self.driver.find_element_by_css_selector('map#rx')
         fields = field_map.find_elements_by_css_selector('area')
         cvf = list(map(lambda x: {
-            'id': x.get_attribute('href').split('=')[1],
+            'id': int(x.get_attribute('href').split('=')[1]),
             'type': x.get_attribute('alt').split(' Level ')[0],
-            'level': x.get_attribute('alt').split(' Level ')[1]
+            'level': int(x.get_attribute('alt').split(' Level ')[1])
         }, list(filter(lambda x: 'id' in x.get_attribute('href'), fields))))
         return cvf
+
+    def get_current_village_buildings(self):
+        building_map = self.driver.find_element_by_css_selector('map#clickareas')
+        buildings = building_map.find_elements_by_css_selector('area')
+        cvb = list(map(lambda x: {
+            'id': int(x.get_attribute('href').split('=')[1]),
+            'type': 'empty' if x.get_attribute('alt') == 'Building site' else None,
+            'level': 0 if x.get_attribute('alt') == 'Building site' else None,
+            'alt': None if x.get_attribute('alt') == 'Building site' else x.get_attribute('alt')
+        }, buildings))
+        for item in cvb:
+            if not item['type']:
+                alt_soup = BeautifulSoup(item['alt'], 'lxml')
+                item['type'] = list(alt_soup.find('p').strings)[0].strip()
+                item['level'] = int(alt_soup.find('span', class_='level').string.split(' ')[1])
+                item['alt'] = None
+        return cvb
+
+    def get_current_village_upgrading(self):
+        anchor = self.driver.find_elements_by_css_selector('h5')
+        if len(anchor) == 0:
+            cvu = []
+            return cvu
+        else:
+            anchor = anchor[0]
+            upgrades = anchor.find_elements_by_xpath('../ul/li')
+            cvu = list(map(lambda x: {
+                'type': x.find_element_by_css_selector('div.name').text.split(' Level ')[0].strip(),
+                'level': int(x.find_element_by_css_selector('div.name').text.split(' Level ')[1].strip()),
+                'finish': x.find_element_by_css_selector('div.buildDuration').text.split(' ')[-1].strip()
+            }, upgrades))
+            return cvu
 
     def logout(self):
         btn_logout = self.driver.find_element_by_xpath('//a[@href="logout.php"]')
@@ -112,9 +164,6 @@ class Travian(object):
 if __name__ == '__main__':
     t = Travian('', '', '')
     t.login()
-    print(t.get_current_village_production())
-    print(t.get_current_village_troops())
-    print(t.get_current_village_resources())
-    print(t.get_current_village_fields())
+    pprint(t.get_current_village_buildings())
     t.logout()
     t.close_browser()
